95c3735a481c5561cb425a1044966df7b0da8d18
James Chartrand <jc.chartrand@gmail.com>
2025-03-17 17:26:48 -0400

    add vc upload form

---
+++ src/app/lib/uploadAction.ts
@@ -0,0 +1,62 @@
+'use server';
+
+import { z } from 'zod';
+
+
+const FormSchema = z.object({
+  vcText: z.string().trim()
+    .min(1, { message: "You must provide a Verifiable Credential." })
+});
+
+export type State = {
+  errors?: {
+    vcText?: string[];
+  };
+  message?: string | null;
+  success?: boolean | null;
+  data?: {
+    vcText?: string;
+  };
+};
+
+
+export async function submitVC(prevState: State, formData: FormData) : Promise<State> {
+
+  const vcText = formData.get('vcText')
+  const data = {vcText}
+  const validatedFields = FormSchema.safeParse(data);
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields - complete them so we can get you your credential!"
+    };
+  }
+
+  try {
+    // validate VC
+         await validateVC(validatedFields.data.vcText)
+         // and here is where we'll send it off to ORCID
+   
+  } catch (error) {
+    console.log(error)
+    return {
+      message: 'Error: Failed to validate.',
+      success: false,
+      ...data
+    };
+  }
+  // const orcidSubmission = getORCIDPayload(validatedFields.data.vcText)  
+  // will need to temporarily store the payload for later submission to API after authenticating.
+  // and somewhere around here we'll redirect to the ORCID auth page.
+  return {...data, success: true};
+}
+
+async function validateVC(vcText:string) {
+    // here is where the vc will be validated
+}
+
+
+
+
---
+++ src/app/ui/upload-form.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import {
+  UserCircleIcon
+} from '@heroicons/react/24/outline';
+
+import { Button } from '@/app/ui/button';
+import { submitVC, State } from '@/app/lib/uploadAction';
+import { useActionState, useState } from 'react';
+
+export default function UploadForm() {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(submitVC, initialState);
+  const [success, setSuccess] = useState(false)
+
+  return (
+    <div>
+
+{ !state.success &&
+
+    <form action={formAction} id="blah">
+      <div className="rounded-md bg-gray-50 p-2 md:p-6">
+       
+  {/* Text area into which to paste the credential */}
+        <div className="mb-1 md:mb-4">
+          <label htmlFor="vcText" className="mb-2 block text-sm font-medium">
+            Name
+          </label>
+          <div className="relative mt-1 md:mt-2 rounded-md">
+            <div className="relative">
+              <textarea
+                id="vcText"
+                name="vcText"
+                cols={40}
+                rows={10}
+                placeholder="Paste your verifiable credential here."
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="vcTest-error"
+              />
+              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+
+          <div id="vcText-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.vcText &&
+              state.errors.vcText.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+      {/* Any returned messages, i.e., errors */}
+        <div aria-live="polite" aria-atomic="true">
+          {state.message ? (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          ) : null}
+        </div>
+      </div>
+
+      {/* The submit button */}
+      <div className="mt-4 md:mt-6 flex justify-center md:gap-4">
+        <Button className="bg-[#429EA6] hover:bg-gray-200 text-gray-900" type="submit">Add Credential to ORCID</Button>
+      </div>
+    </form>
+}
+
+    { state.success &&
+
+      <div className=" max-w-[500px] space-y-2.5 p-4 md:-mt-10 text-center">
+        <br/><br/>
+        Your credential has been submitted!
+        <br/><br/>
+        You should momentarily be redirected to the ORCID login page.
+        <br/><br/>
+        <div className="mt-2 md:mt-6 flex justify-center md:gap-4">
+          <Button className="bg-[#429EA6] hover:bg-gray-200 text-gray-900" onClick={()=>{state.success = false; setSuccess(!success)}}>Cancel</Button>
+        </div>
+      </div>
+    }
+</div>
+
+    
+  );
+}
