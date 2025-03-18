c41b2fc8daf80727bcec872bb7923e89244488a8
James Chartrand <jc.chartrand@gmail.com>
2025-03-18 07:56:49 -0400

    add module declarations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "include": [
+    "src/modules.d.ts"
+  ]
 };
 
 export default nextConfig;
---
+++ src/types/modules.d.ts
@@ -0,0 +1,1 @@
+declare module '@digitalcredentials/verifier-core';
