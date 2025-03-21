502d7f80c3cb43f508c98111f5e5aebd323facfb
James Chartrand <jc.chartrand@gmail.com>
2025-03-21 10:04:58 -0400

    separate upload from login

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import { Button } from '@/app/ui/button';
-import UploadForm from "./ui/upload-form";
+import UploadForm from "../ui/upload-form";
 
 export default function Home() {
   return (
@@ -14,16 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        Add your credential to your ORCID profile.
-
-        You will first login to ORCID to authorize the addition. 
-
-       {/* The login button */}
-       <div className="mt-4 md:mt-6 flex justify-center md:gap-4">
-        <Button className="bg-[#429EA6] hover:bg-gray-200 text-gray-900" type="submit">Login to ORCID</Button>
-      </div>
-
-
+        <UploadForm/>
 
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
