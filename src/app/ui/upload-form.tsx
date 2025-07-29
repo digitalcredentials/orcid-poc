565156cd7a249b14973180ee84f09215be4a0d70
James Chartrand <jc.chartrand@gmail.com>
2025-07-29 12:57:36 -0400

    fix text color for dark mode

--- src/app/ui/upload-form.tsx
+++ src/app/ui/upload-form.tsx
@@ -76,7 +76,7 @@ export default function UploadForm() {
                     rows={10}
                     placeholder="Paste your verifiable credential here."
                     defaultValue={credential}
-                    className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 bg-gray-50 placeholder:text-blue-500 text-black"
                     aria-describedby="vcTest-error"
                   />
                 </div>
